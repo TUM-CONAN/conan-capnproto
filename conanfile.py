7b10fbe8713ee110c4cc30954d6df5494199b13a
Ulrich Eck <ulrich.eck@tum.de>
2020-04-25 10:40:30 +0200

    adapted to camp conan environment - migrated to cmake build for platform independence

--- conanfile.py
+++ conanfile.py
@@ -1,22 +1,29 @@
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from conans import CMake, ConanFile, tools
 
 
 class CapNProtoConan(ConanFile):
     name = "capnproto"
-    version = tools.get_env("GIT_TAG", "0.7.0")
+    version = tools.get_env("GIT_TAG", "0.8.0")
     license = "MIT"
     description = "Cap'n Proto serialization/RPC system"
     settings = "os", "arch", "compiler", "build_type"
-
-    def build_requirements(self):
-        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+    generators = "cmake"
 
     def source(self):
         tools.get("https://capnproto.org/capnproto-c++-%s.tar.gz" % self.version)
 
+    def _cmake_configure(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="capnproto-c++-%s" % self.version)
+        return cmake
+
     def build(self):
-        with tools.chdir("capnproto-c++-%s" % self.version):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure()
-            autotools.make()
-            autotools.install()
+        cmake = self._cmake_configure()
+        cmake.build()
+
+    def package(self):
+        cmake = self._cmake_configure()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.lib = tools.collect_libs(self)
