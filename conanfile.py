668d6deefb10d59fdc8386161f4d6eb056991c81
Ulrich Eck <ulrich.eck@tum.de>
2020-05-08 08:43:28 +0200

    add shared option

--- conanfile.py
+++ conanfile.py
@@ -8,12 +8,30 @@ class CapNProtoConan(ConanFile):
     description = "Cap'n Proto serialization/RPC system"
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake"
+    options = {
+        "shared": [True, False],
+    }
+
+    default_options = {
+        "shared": True,
+    }
 
     def source(self):
         tools.get("https://capnproto.org/capnproto-c++-%s.tar.gz" % self.version)
 
     def _cmake_configure(self):
         cmake = CMake(self)
+        cmake.verbose = True
+
+        def add_cmake_option(option, value):
+            var_name = "{}".format(option).upper()
+            value_str = "{}".format(value)
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            cmake.definitions[var_name] = var_value
+
+        for option, value in self.options.items():
+            add_cmake_option(option, value)
+
         cmake.configure(source_folder="capnproto-c++-%s" % self.version)
         return cmake
 
